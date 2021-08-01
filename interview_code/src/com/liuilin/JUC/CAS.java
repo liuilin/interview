0925e83ef72b7829971b1c9b60f8ae961de6995a
liuqiang <liu1142865396@gmail.com>
2021-08-01 22:51:12 +0800

    update

---
+++ interview_code/src/com/liuilin/JUC/CAS.java
@@ -0,0 +1,18 @@
+package com.liuilin.JUC;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * CAS: compare and swap
+ *
+ * @author liuqiang
+ * @since 2021-07-31
+ */
+public class CAS {
+
+    public static void main(String[] args) {
+        AtomicInteger atomicInteger = new AtomicInteger(10);
+        System.out.println(atomicInteger.compareAndSet(10, 9) + " current value: " + atomicInteger.get());
+        System.out.println(atomicInteger.compareAndSet(10, 8) + " current value: " + atomicInteger.get());
+    }
+}
