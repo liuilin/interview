866b47fc229a4f451a78540f9fd58631babeb740
liuqiang <liu1142865396@gmail.com>
2021-08-09 08:41:00 +0800

    删除垃圾图片

--- interview_code/src/com/liuilin/JUC/LazySingleton.java
+++ interview_code/src/com/liuilin/JUC/LazySingleton.java
@@ -10,9 +10,9 @@ public class LazySingleton {
 
     // 这里用 static是因为 getInstance() 方法是静态的，而静态方法不能访问非静态成员变量，所以instance必须是静态成员变量
     // getInstance() 方法是静态是因为构造器是私有的，只能通过 Singleton.getInstance() 方法获取对象实例
-    private static LazySingleton instance = null;
+    private static volatile LazySingleton instance = null;
 
-    // 构造器是私有是为了防止其他类通过 new Singleton() 来创建对象实例
+    // 构造器是私有是为了防止其他类通过 new LazySingleton() 来创建对象实例
     private LazySingleton() {
         System.out.println(Thread.currentThread().getName() + " --- 我是构造方法");
     }
